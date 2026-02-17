183f48db1c166d376ae1382e5395aef1616c86a7
Assil Kalakesh <assilkalakesh02@hotmail.com>
2026-02-17 12:54:01 +0200

    update1

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,7 @@
 export default function Login() {
   return (
     <div style={{ padding: 20 }}>
-      <h1>Login</h1>
-      <p>User login page.</p>
+      <title>TripMaster | SignMY BRANCH</title>
     </div>
   );
 }
