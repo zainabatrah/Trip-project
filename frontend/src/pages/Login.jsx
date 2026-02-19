7248baa8c2c3eca219dd564703e4d032a369d48d
Assil Kalakesh <assilkalakesh02@hotmail.com>
2026-02-19 17:06:22 +0200

    Add login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,69 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 export default function Login() {
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
-    <div style={{ padding: 20 }}>
-      <title>TripMaster | SignMY BRANCH</title>
-    </div>
+<>
+        {/* Header */}
+        <div >
+          <h1>Welcome Back</h1>
+          <p>
+            LOGO
+          </p>
+        </div>
+
+        {/* Form */}
+        <form>
+          {/* Email */}
+          <div>
+            <label>Email</label>
+            <input
+              type="email"
+              placeholder="example@email.com"
+            />
+          </div>
+
+          {/* Password */}
+          <div>
+            <label>Password</label>
+            <div>
+              <input
+                type={showPassword ? "text" : "password"}
+                placeholder="********"
+               />
+              <button
+                type="button"
+                onClick={() => setShowPassword(!showPassword)}
+              >
+                {showPassword ? "Hide" : "Show"}
+              </button>
+            </div>
+          </div>
+
+          {/* Remember */}
+          <div>
+            <input type="checkbox" />
+            <span>
+              Remember me
+            </span>
+          </div>
+
+          {/* Button */}
+          <button>
+            Sign In
+          </button>
+        </form>
+
+        {/* Footer */}
+        <p>
+          Don’t have an account?{" "}
+          <Link to="/register" className="text-blue-600 font-semibold">
+          Sign up
+          </Link>
+
+        </p>
+</>
   );
 }
