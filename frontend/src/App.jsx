faab17716bfd3645269135946f81df70d6cdc549
Maya Issa <mayahaniissa0123456789@gmail.com>
2026-07-08 23:06:46 +0300

    Resolve conflict

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,34 +4,28 @@ import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Trips from "./pages/Trips";
 import PrivateTrip from "./pages/PrivateTrip";
-<<<<<<< HEAD
+import Welcome from "./pages/Welcome";
 import TripDetails from "./pages/TripDetails";
-=======
+
 import About from "./pages/About";
 
-// Use these only if the files exist
-// import Dashboard from "./pages/Dashboard";
-// import Bookings from "./pages/Bookings";
-// import Profile from "./pages/Profile";
-
->>>>>>> fa7baa441c87e0ddddde0572c6d9f225780bd353
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/trips" replace />} />
+      <Route path="/" element={<Welcome/>} />
 
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
       <Route path="/trips" element={<Trips />} />
       <Route path="/private-trip" element={<PrivateTrip />} />
       <Route path="/about" element={<About />} />
-<<<<<<< HEAD
+
  <Route
           path="/trips/:id"
           element={<TripDetails />}
         />
       <Route path="*" element={<Navigate to="/" replace />} />
-=======
+
 
       {/* Uncomment only if these pages exist */}
       {/* <Route path="/dashboard" element={<Dashboard />} /> */}
@@ -39,7 +33,7 @@ export default function App() {
       {/* <Route path="/profile" element={<Profile />} /> */}
 
       <Route path="*" element={<Navigate to="/trips" replace />} />
->>>>>>> fa7baa441c87e0ddddde0572c6d9f225780bd353
+
     </Routes>
   );
 }
